46005d807f4f9e365e61c9de213e903a7cd873bf
Eli Cohavi <cohavi.eli@gmail.com>
2025-08-05 01:06:25 -0500

    Added Object class and controller class that contains static list of tasks.

---
+++ src/main/java/com/elicohavi/task_api/TaskController.java
@@ -0,0 +1,36 @@
+package com.elicohavi.task_api;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+
+@RestController
+public class TaskController {
+
+    private ArrayList<Task> tasks = new ArrayList<>();
+
+    private AtomicInteger idCounter = new AtomicInteger(1);
+
+    public TaskController() {
+        Task task1 = new Task(1, "Dishes",
+                "Unload Dishwasher and load sink dishes.", false);
+        tasks.add(task1);
+        Task task2 = new Task(2, "Laundry",
+                "Switch loads and hang up clean clothes.", false);
+        tasks.add(task2);
+        Task task3 = new Task(3, "Gas",
+                "Fill up gas tank and handheld can.", false);
+        tasks.add(task3);
+        Task task4 = new Task(4, "Doctor",
+                "Schedule Doctor appointment", false);
+        tasks.add(task4);
+    }
+    @GetMapping("/tasks")
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+}
