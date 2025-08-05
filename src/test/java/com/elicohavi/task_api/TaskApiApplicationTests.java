0538eae2df0211b1f3435453a151f6fedc6abd60
Eli Cohavi <cohavi.eli@gmail.com>
2025-08-04 20:23:02 -0500

    Initial Commit: Spring Boot Task API

---
+++ src/test/java/com/elicohavi/task_api/TaskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elicohavi.task_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
